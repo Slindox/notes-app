102ebaa7bb7b2a9b5cdb8fab87ca6d4f6d2f7972
Léon Mikeleit <mikeleit@sport-thieme.de>
2020-03-29 21:16:07 +0200

    semicolons removed

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
-const fs = require('fs');
-const chalk = require('chalk');
-const yargs = require('yargs');
-const getNotes = require('./notes.js');
+const fs = require('fs')
+const chalk = require('chalk')
+const yargs = require('yargs')
+const getNotes = require('./notes.js')
 
 // Customize yargs version
-yargs.version('1.1.0');
+yargs.version('1.1.0')
 
 // Creade add command
 yargs.command({
@@ -23,36 +23,36 @@ yargs.command({
     }
   },
   handler: function(argv) {
-    console.log('Title: ' + argv.title);
-    console.log('Body: ' + argv.body);
+    console.log('Title: ' + argv.title)
+    console.log('Body: ' + argv.body)
   }
-});
+})
 
 // Creade rm command
 yargs.command({
   command: 'rm',
   describe: 'Removing a new note',
   handler: function() {
-    console.log('Removed a new note!');
+    console.log('Removed a new note!')
   }
-});
+})
 
 // Creade read command
 yargs.command({
   command: 'read',
   describe: 'Read a note',
   handler: function() {
-    console.log('Readed note');
+    console.log('Readed note')
   }
-});
+})
 
 // Creade ls command
 yargs.command({
   command: 'ls',
   describe: 'List all notes',
   handler: function() {
-    console.log('List all notes');
+    console.log('List all notes')
   }
-});
+})
 
-yargs.parse();
+yargs.parse()
