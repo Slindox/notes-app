66c3d3d69c78bf341434f09bf339ffa589f80d50
Léon Mikeleit <mikeleit@sport-thieme.de>
2020-04-04 10:07:25 +0200

    refactored to arrow functions

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ yargs.command({
       type: 'string',
     },
   },
-  handler: function (argv) {
+  handler(argv) {
     notes.addNote(argv.title, argv.body)
   },
 })
@@ -38,7 +38,7 @@ yargs.command({
       type: 'string',
     },
   },
-  handler: function (argv) {
+  handler(argv) {
     notes.removeNote(argv.title)
   },
 })
@@ -47,7 +47,7 @@ yargs.command({
 yargs.command({
   command: 'read',
   describe: 'Read a note',
-  handler: function () {
+  handler() {
     console.log('Readed note')
   },
 })
@@ -56,7 +56,7 @@ yargs.command({
 yargs.command({
   command: 'ls',
   describe: 'List all notes',
-  handler: function () {
+  handler() {
     console.log('List all notes')
   },
 })
--- notes.js
+++ notes.js
@@ -3,15 +3,14 @@ const chalk = require('chalk')
 const success = chalk.bold.green
 const error = chalk.bold.red
 
-const getNotes = function () {
+const getNotes = () => {
   return 'Your notes...'
 }
 
-const addNote = function (title, body) {
+const addNote = (title, body) => {
   const notes = loadNotes()
-  const duplicateNotes = notes.filter(function (note) {
-    return note.title === title
-  })
+  const duplicateNotes = notes.filter((note) => note.title === title)
+
   if (duplicateNotes.length === 0) {
     notes.push({
       title: title,
@@ -24,11 +23,9 @@ const addNote = function (title, body) {
   }
 }
 
-const removeNote = function (title) {
+const removeNote = (title) => {
   const notes = loadNotes()
-  const notesToKeep = notes.filter(function (note) {
-    return note.title !== title
-  })
+  const notesToKeep = notes.filter((note) => note.title !== title)
   if (notes.length > notesToKeep.length) {
     console.log(success('Removed note!'))
     saveNotes(notesToKeep)
@@ -37,12 +34,12 @@ const removeNote = function (title) {
   }
 }
 
-const saveNotes = function (notes) {
+const saveNotes = (notes) => {
   const dataJSON = JSON.stringify(notes)
   fs.writeFileSync('notes.json', dataJSON)
 }
 
-const loadNotes = function () {
+const loadNotes = () => {
   try {
     const dataBuffer = fs.readFileSync('notes.json')
     const dataJSON = dataBuffer.toString()
